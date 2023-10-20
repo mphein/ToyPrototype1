d86f5cbac0a6cf12a252b674fc63ce26061460be
Michael Hein <mphein@ucsc.edu>
2023-10-20 13:32:04 -0700

    added one ghost and adjusting character

--- src/scenes/Overworld.js
+++ src/scenes/Overworld.js
@@ -2,15 +2,24 @@ class Overworld extends Phaser.Scene {
   constructor() {
     super({key:'overworldScene'})
 
-    this.VEL = 25
+    this.VEL = 100
   }
 
   preload() {
     this.load.path = './assets/'
-    this.load.spritesheet('slime', 'slime.png', {
+    this.load.audio('gameBackground', 'Background.mp3');
+
+    this.load.spritesheet('ghost', 'ghost.png' , {
+      frameWidth: 32,
+      frameHeight: 32
+    })
+    
+    this.load.spritesheet('character', 'Character.png', {
       frameWidth: 16,
       frameHeight: 16
     })
+
+  
     this.load.image('tilesetImage', 'tileset.png')
     this.load.tilemapTiledJSON('tilemapJSON', 'area01.json')
 
@@ -25,30 +34,38 @@ class Overworld extends Phaser.Scene {
     const terrainLayer = map.createLayer('Terrain', tileset, 0, 0)
 
     // add player
-    this.slime = this.physics.add.sprite(32, 32, 'slime', 0)
+    this.character = this.physics.add.sprite(545, 90, 'character', 0)
+
+
+    // add ghosts
+    //
+    this.ghost1 = this.physics.add.sprite(545, 200, 'ghost')
     this.anims.create({
-      key: 'jiggle',
+      key: 'spook',
       frameRate: 8,
       repeat: -1,
-      frames: this.anims.generateFrameNumbers('slime', {
-        start:0,
-        end:1
+      frames: this.anims.generateFrameNumbers('ghost', {
+        start: 0,
+        end: 3
       })
     })
-    this.slime.play('jiggle')
+    this.ghost1.play('spook')
+
     
+
+    // play music
+    // pixabay SoulProdMusic Sinister Night / Halloween Trap Hip Hop Music
+    this.sound.play('gameBackground', {volume: .25, loop: true});
 
     // enable collision
     terrainLayer.setCollisionByProperty({collides:true})
-    this.physics.add.collider(this.slime, terrainLayer)
+    this.physics.add.collider(this.character, terrainLayer)
 
     // cameras
     this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels)
-    this.cameras.main.startFollow(this.slime, true, 0.25, 0.25)
+    this.cameras.main.startFollow(this.character, true, 0.25, 0.25)
     this.physics.world.bounds.setTo(0,0, map.widthInPixels, map.heightInPixels)
-
-    this.slime.body.setCollideWorldBounds(true)
-
+    this.cameras.main.setZoom(1.5,1.5)
 
     //input 
     this.cursors = this.input.keyboard.createCursorKeys()
@@ -67,6 +84,6 @@ class Overworld extends Phaser.Scene {
       this.direction.y = 1
     }
     this.direction.normalize()
-    this.slime.setVelocity(this.VEL * this.direction.x, this.VEL * this.direction.y)
+    this.character.setVelocity(this.VEL * this.direction.x, this.VEL * this.direction.y)
   }
 }
